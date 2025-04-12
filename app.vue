58f1d931cb693b9631663123f0adc0c20457482e
laila-terrak <lailaterrak@gmail.com>
2025-04-12 14:04:58 +0200

    feat(ui): integrate nuxt-ui components

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="p-8">
+    <UButton icon="i-lucide-rocket" size="md" color="primary" variant="solid">Button</UButton>
+  </div>
   </div>
 </template>
